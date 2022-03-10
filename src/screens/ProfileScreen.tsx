db09ac06ab37a8b38f4fbfc4af5bc4f31d83f488
Subin Chaliyath <64072829+subinchaliyath@users.noreply.github.com>
2022-03-10 11:21:29 +0530

    feat: profile page without css

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from 'react'
+
+import request from '../api'
+
+type User = {
+  id: number
+  name: string
+  email: string
+}
+
 const ProfileScreen = () => {
-  return <div>ProfileScreen</div>
+  const [user, setUser] = useState<User>({ id: 0, name: '', email: '' })
+  const [disabled, setDisabled] = useState(true)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const name = e.target.name
+    setUser({ ...user, [name]: e.target.value })
+  }
+  useEffect(() => {
+    void (async () => {
+      try {
+        const { data } = await request.get<User>('/user')
+        setUser(data)
+      } catch (error: unknown) {
+        console.log(error)
+      }
+    })()
+  }, [])
+  return (
+    <>
+      {user.email ? (
+        <>
+          <form>
+            <input
+              type="text"
+              name="name"
+              value={user.name}
+              disabled={disabled}
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              type="email"
+              name="email"
+              value={user.email}
+              disabled={disabled}
+              onChange={handleChange}
+            />
+          </form>
+          <button onClick={() => setDisabled(false)}> Edit</button>
+          <button onClick={() => setDisabled(true)}> Update</button>
+        </>
+      ) : null}
+    </>
+  )
 }
 
 export default ProfileScreen
