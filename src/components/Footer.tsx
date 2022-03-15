770de3f69b9049e89b1b7b202eb7ffa818fbe8f3
Subin Chaliyath <64072829+subinchaliyath@users.noreply.github.com>
2022-03-16 01:47:58 +0530

    feat:footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,20 @@
+import styled from '@emotion/styled'
+import { lightdark } from '../styles/colors'
+
+const FooterContainer = styled.footer`
+  p {
+    padding: 20px 0;
+    text-align: center;
+    background-color: ${lightdark};
+  }
+`
+
 const Footer: React.FC = () => {
-  return <footer>Footer</footer>
+  return (
+    <FooterContainer>
+      <p>Copyright &copy; The Cozy Library</p>
+    </FooterContainer>
+  )
 }
 
 export default Footer
