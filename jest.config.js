8d213b540037563402d7a82b482ba4faa8ddc4c7
Subin Chaliyath <64072829+subinchaliyath@users.noreply.github.com>
2022-03-21 10:58:46 +0530

    fix: testing with svg import

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
   setupFilesAfterEnv: ['./src/jest.setup.ts'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/src/__mocks__/fileMock.ts',
     '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js',
   },
 }
